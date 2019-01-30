b386ab48e61ff67d1c08eefbba0f4f036755a855
刘颖 <1716871990@qq.com>
2019-01-30 21:33:48 +0800

    不知道为什么p155的代码5-11输出答案和书上不一样

---
+++ src/Chap05/CariageReturn.java
@@ -0,0 +1,8 @@
+package Chap05;
+
+public class CariageReturn {
+    public static void main(String[] args) {
+        System.out.print("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+        System.out.println("\r12345");
+    }
+}
